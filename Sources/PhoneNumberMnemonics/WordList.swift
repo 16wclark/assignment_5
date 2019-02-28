54ce71bb16238e4d32a98361de8a6c8cf4c29206
Johnson <elijah.johnson@120-148.champlain.edu>
2019-02-28 16:38:42 -0500

    Added readFromFile and wordsOfLength functions (untested)

--- Sources/PhoneNumberMnemonics/WordList.swift
+++ Sources/PhoneNumberMnemonics/WordList.swift
@@ -6,3 +6,14 @@ import Foundation
 // YOU FILL IN HERE
 // You'll want to create a mechanism here for loading and querying
 // words.txt. It's up to you how you do this. You may consider a struct.
+public func readFromFile(path: String) -> [String] {
+    let text = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+    let lines : [String] = text.components(separatedBy: "\n")
+    return lines
+}
+
+public func wordsOfLength(path: String, length: Int) -> [String] {
+    let text = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+    let lines : [String] = text.components(separatedBy: "\n")
+    let words = lines.filter { $0.count == length }
+}
