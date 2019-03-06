a8198d8a5daaf01ca970657fb161b2e9309f362a
Johnson <elijah.johnson@120-148.champlain.edu>
2019-03-05 21:02:33 -0500

    Added header comments

--- Sources/PhoneNumberMnemonics/WordList.swift
+++ Sources/PhoneNumberMnemonics/WordList.swift
@@ -1,3 +1,22 @@
+/*
+ Author: EJ Johnson & Will Clark
+ Class: CSI-380-01
+ Assignment: Assignment 5
+ Date Assigned: 02/21
+ Due Date: 03/05 11:59pm
+ Description:
+ This program gets all the possible fruit mnemonics for a phone number
+ Certification of Authenticity:
+ I certify that this is entirely my own work, except where I have given
+ fully-documented references to the work of others. I understand the definition and
+ consequences of plagiarism and acknowledge that the assessor of this assignment
+ may, for the purpose of assessing this assignment:
+ - Reproduce this assignment and provide a copy to another member of academic
+ - staff; and/or Communicate a copy of this assignment to a plagiarism checking
+ - service (which may then retain a copy of this assignment on its database for the
+ - purpose of future plagiarism checking)
+ */
+
 // WordList.swift
 // Created for Assignment 5 of CSI 380
 
